6fb88d6cfe1dff0996e79fa48064f0439261a8ac
Simone Cimarelli <aquilairreale@ymail.com>
2019-02-26 14:55:29 +0100

    Remove colons

--- strings/en.h
+++ strings/en.h
@@ -2,9 +2,9 @@
 #define _S_EN_H_
 
 #define S_ENTER_TIMEOUT     "ENTER TIMEOUT"
-#define S_TIMEOUT_ENTERED   "TIMEOUT CHOSEN:"
+#define S_TIMEOUT_ENTERED   "TIMEOUT CHOSEN"
 #define S_ENTER_PIN         "ENTER PIN"
-#define S_PIN_ENTERED       "PIN CHOSEN:"
+#define S_PIN_ENTERED       "PIN CHOSEN"
 #define S_TIME              "TIME:"
 #define S_DEFUSED_1         "*     BOMB     *"
 #define S_DEFUSED_2         "*   DEFUSED    *"
--- strings/it.h
+++ strings/it.h
@@ -2,9 +2,9 @@
 #define _S_IT_H_
 
 #define S_ENTER_TIMEOUT     "INSERIRE TEMPO"
-#define S_TIMEOUT_ENTERED   "TEMPO INSERITO:"
+#define S_TIMEOUT_ENTERED   "TEMPO INSERITO"
 #define S_ENTER_PIN         "INSERIRE CODICE"
-#define S_PIN_ENTERED       "CODICE INSERITO:"
+#define S_PIN_ENTERED       "CODICE INSERITO"
 #define S_TIME              "TEMPO:"
 #define S_DEFUSED_1         "*    BOMBA     *"
 #define S_DEFUSED_2         "* DISINNESCATA *"
